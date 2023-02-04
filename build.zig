7525e280f356241d76ab87f6a0cf6e7ac1121a9c
Louis Pearson <opensource@louispearson.work>
2023-02-03 17:31:12 -0700

    Cherrypick build: depend on external cf package

--- build.zig
+++ build.zig
@@ -1,20 +1,23 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const cf_pkg = b.dependency("cf", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
-    const exe = b.addExecutable("jazmin", "src/main.zig");
-    exe.addPackagePath("cf", "dep/cf/cf.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    const cf_module = cf_pkg.module("cf");
+
+    const exe = b.addExecutable(.{
+        .name = "jazmin",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.addModule("cf", cf_module);
     exe.install();
 
     const run_cmd = exe.run();
@@ -26,10 +29,12 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.addPackagePath("cf", "dep/cf/cf.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
+    const exe_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    exe_tests.addModule("cf", cf_module);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
